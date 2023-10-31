682404d8dbf40ea5a7f02ad260856e68c54af968
geonhee-youha <geonhee.lee@youha.info>
2023-10-31 15:04:40 +0900

    메인화면 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,34 @@
-import router from "next/router";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import useAxios from "../hooks/swrs";
+import { Container } from "@mui/material";
+import Button from "../components/atoms/Button";
 
 export default function App() {
-  useEffect(() => {
-    // useAxios(`/user/me`).then((res) => {
-    //   if (res) {
-    //     if (res.status === 200) {
-    //       router.push(`/main`);
-    //     } else {
-    //       router.push(`/auth`);
-    //     }
-    //   }
-    // });
-    router.push(`/main`); // TEMP (앱인만큼 로그인 체크 필요)
-  }, []);
-  return <></>;
+  const router = useRouter();
+  // useEffect(() => {
+  //   // useAxios(`/user/me`).then((res) => {
+  //   //   if (res) {
+  //   //     if (res.status === 200) {
+  //   //       router.push(`/main`);
+  //   //     } else {
+  //   //       router.push(`/auth`);
+  //   //     }
+  //   //   }
+  //   // });
+  //   router.push(`/main`); // TEMP (앱인만큼 로그인 체크 필요)
+  // }, []);
+  return (
+    <Container
+      sx={{
+        width: "100%",
+        height: "100vh",
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button size='lg' onClick={() => router.push(`/main`)}>메인으로</Button>
+    </Container>
+  );
 }
